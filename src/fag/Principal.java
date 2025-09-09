2503a4433e5b10df6bd969e206a79696c22ea84d
luiz_fernando_de_souza_rossato <lr265510@gmail.com>
2025-09-09 02:19:11 +0000

    calculadora

--- src/fag/Principal.java
+++ src/fag/Principal.java
@@ -4,22 +4,37 @@ public class Principal {
 
 	public static void main(String[] args) {
 	
-         
-      Scanner scanner = new Scanner(System.in);
-      System.out.print("Qual o seu nome? ");
-      String nome = scanner.nextLine();
-      System.out.print("Qual a sua idade? ");
-      int idade = scanner.nextInt();
+       Scanner scanner = new Scanner(System.in);
+      System.out.println( "valor de a");
+      long a = scanner.nextLong();
       scanner.nextLine();
-      System.out.print("Qual o seu cpf? ");
-      String cpf = scanner.nextLine();
-       
-
-      System.out.println("Olá " + nome + "!");
-      System.out.println("Você tem " + idade + " anos");
-      System.out.println("Seu cpf é " + cpf + "!");
+      System.out.println( " Valor de b");
+      long b = scanner.nextLong();
+      scanner.nextLine();
+      System.out.println( "selecione o operador");
+      String operador = scanner.nextLine();
       
-      scanner.close();
+     if(operador.equals("+")){ 
+         System.out.println("Seu resultado é "+(a+b));
+    }
+    
+   else if (operador.equals("-")){
+       System.out.println("Seu resultado é "+ (a-b));
+   }
+   
+   else if (operador.equals("*")){
+       System.out.println("Seu resultado é "+ (a*b));
+   }
+   
+   else if(operador.equals("/")){
+      System.out.println("Seu resultado é "+(a/b));
+   }
+   
+
+    scanner.close();
+     
+     }
+ }
  
     }
   
