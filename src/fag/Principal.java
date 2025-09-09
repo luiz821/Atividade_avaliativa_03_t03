8ec7c170c8a5f5516ac190b93fcffd8028bbad3c
luiz_fernando_de_souza_rossato <lr265510@gmail.com>
2025-09-09 02:13:46 +0000

    primeira atvidade chat

--- src/fag/Principal.java
+++ src/fag/Principal.java
@@ -3,7 +3,27 @@ package fag;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+         
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Qual o seu nome? ");
+      String nome = scanner.nextLine();
+      System.out.print("Qual a sua idade? ");
+      int idade = scanner.nextInt();
+      scanner.nextLine();
+      System.out.print("Qual o seu cpf? ");
+      String cpf = scanner.nextLine();
+       
+
+      System.out.println("Olá " + nome + "!");
+      System.out.println("Você tem " + idade + " anos");
+      System.out.println("Seu cpf é " + cpf + "!");
+      
+      scanner.close();
+ 
+    }
+  
+}
 
 	}
 
